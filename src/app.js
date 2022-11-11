c56ad6fd2057494c003029872ac1bec32980b6ba
BrayanSO <briansolera@gmail.com>
2022-11-11 02:59:10 +0000

    segunda parte

--- src/app.js
+++ src/app.js
@@ -6,6 +6,10 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generatecart();
 };
+
+function generatecart() {
+  let figures = ["♦", "♥", "♠", "♣"];
+  let num = Math.floor(Math.random() * 3);
+}
