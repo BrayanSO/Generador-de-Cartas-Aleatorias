f5360ab4893feba2d7a4069cfa81005878968b84
BrayanSO <briansolera@gmail.com>
2022-11-21 16:06:23 +0000

    trabajo final

--- src/app.js
+++ src/app.js
@@ -6,10 +6,35 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  generatecart();
+  document.querySelector(".card").classList.add(generateRandomSuit("♦"));
+  document.querySelector(".number").innerHTML = generateRandomNumber();
 };
 
-function generatecart() {
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let iNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[iNumbers];
+};
+
+let generateRandomSuit = () => {
   let figures = ["♦", "♥", "♠", "♣"];
-  let num = Math.floor(Math.random() * 3);
-}
+  let numfig = Math.floor(Math.random() * 3);
+  let figure = figures[numfig];
+  let color = numfig <= 1 ? "red" : "black";
+
+  document.querySelectorAll(".figure").forEach(elm => (elm.innerHTML = figure));
+};
